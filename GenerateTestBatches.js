7a31567153d6b10da0cc15dff750f9fd56f4d684
Hüseyin Atakan Çiçek <cicekhu@gmail.com>
2019-05-02 13:27:35 +0300

    Add generateTestBatches

--- GenerateTestBatches.js
+++ GenerateTestBatches.js
@@ -3,15 +3,15 @@ const Greedy = require('./Greedy').chooseGreedy;
 
 const fs = require('fs');
 
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 1; i++) {
   let result = [];
-  for (let j = 0; j < 100; j++) {
+  for (let j = 0; j < 1000; j++) {
     const game = new Game();
     result.push(Greedy(game));
   }
 
   if (i % 100 === 0) console.log('Iteration:', i);
 
-  const file = './batches/' + i + '.txt';
+  const file = './testbatch.txt';
   fs.writeFileSync(file, JSON.stringify(result));
 }
