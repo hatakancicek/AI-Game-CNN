5eecdfe96cd746c49e2fbeb3085c7822acf3ba7f
Hüseyin Atakan Çiçek <cicekhu@gmail.com>
2019-05-02 13:26:28 +0300

    Fix tensor leak

--- GenerateBatches.js
+++ GenerateBatches.js
@@ -3,13 +3,15 @@ const Greedy = require('./Greedy').chooseGreedy;
 
 const fs = require('fs');
 
-for(let i = 0; i < 10000; i++) {
+for (let i = 0; i < 1; i++) {
   let result = [];
-  for(let j = 0; j < 100; j++) {
+  for (let j = 0; j < 1000; j++) {
     const game = new Game();
     result.push(Greedy(game));
   }
 
-  const file = "./batches/" + i + ".txt";
+  if (i % 100 === 0) console.log('Iteration:', i);
+
+  const file = './testbatch.txt';
   fs.writeFileSync(file, JSON.stringify(result));
 }
--- index.js
+++ index.js
@@ -3,40 +3,47 @@ const fs = require('fs');
 
 const model = tf.sequential();
 
-model.add(tf.layers.conv2d({
-  inputShape: [8, 8, 6],
-  kernelSize: 3,
-  filters: 16,
-  strides: 1,
-  activation: 'relu',
-  kernelInitializer: tf.initializers.glorotNormal(),
-}));
+model.add(
+  tf.layers.conv2d({
+    inputShape: [8, 8, 6],
+    kernelSize: 3,
+    filters: 16,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: tf.initializers.glorotNormal(),
+  })
+);
 
-model.add(tf.layers.conv2d({
-  kernelSize: 3,
-  filters: 12,
-  strides: 1,
-  activation: 'relu',
-  kernelInitializer: tf.initializers.glorotNormal(),
-}));
+model.add(
+  tf.layers.conv2d({
+    kernelSize: 3,
+    filters: 12,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: tf.initializers.glorotNormal(),
+  })
+);
 
-
-model.add(tf.layers.conv2d({
-  kernelSize: 3,
-  filters: 8,
-  strides: 1,
-  activation: 'relu',
-  kernelInitializer: tf.initializers.glorotNormal(),
-}));
+model.add(
+  tf.layers.conv2d({
+    kernelSize: 3,
+    filters: 8,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: tf.initializers.glorotNormal(),
+  })
+);
 
 model.add(tf.layers.flatten());
 
-model.add(tf.layers.dense({
-  units: 122,
-  kernelInitializer: tf.initializers.glorotNormal(),
-  activation: 'softmax',
-  useBias: true,
-}));
+model.add(
+  tf.layers.dense({
+    units: 122,
+    kernelInitializer: tf.initializers.glorotNormal(),
+    activation: 'softmax',
+    useBias: true,
+  })
+);
 
 const LEARNING_RATE = 0.15;
 const optimizer = tf.train.sgd(LEARNING_RATE);
@@ -52,7 +59,7 @@ const TEST_SIZE = 1000;
 
 const TRAIN_BATCHES = 10000;
 
-const testData = JSON.parse(fs.readFileSync("./testbatch.txt"));
+const testData = JSON.parse(fs.readFileSync('./testbatch.txt'));
 const validationData = {
   states: [],
   labels: [],
@@ -67,31 +74,37 @@ validationData.states = tf.tensor4d(validationData.states);
 
 (async function main() {
   for (let i = 0; i < TRAIN_BATCHES; i++) {
-  
-    const data = JSON.parse(fs.readFileSync("./batches/" + i + ".txt"));
+    const data = JSON.parse(
+      fs.readFileSync('./batches/' + i + '.txt')
+    );
     const batch = {
       states: [],
       labels: [],
     };
-    
+
     data.forEach(({ state, action }) => {
       batch.states.push(state);
       batch.labels.push(action);
     });
+
+    const y = tf.tidy(() => {
+      const history = await model.fit(
+        tf.tensor4d(batch.states),
+        tf.tensor2d(batch.labels),
+        {
+          batchSize: BATCH_SIZE,
+          validationData: [
+            validationData.states,
+            tf.tensor2d(validationData.labels),
+          ],
+          epochs: 100,
+        }
+      );
   
-    const history = await model.fit(
-      tf.tensor4d(batch.states),
-      tf.tensor2d(batch.labels),
-      {
-        batchSize: BATCH_SIZE,
-        validationData: [validationData.states, tf.tensor2d(validationData.labels)],
-        epochs: 1
-      });
+      console.log(i);
+      const loss = history.history.loss[0];
+      const accuracy = history.history.acc[0];
 
-
-    console.log(i);
-    const loss = history.history.loss[0];
-    const accuracy = history.history.acc[0];
-
-  };
+    });
+  }
 })();
